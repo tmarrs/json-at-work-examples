0a0d6afb00854affc2e981307046cfea9369e137
Tom Marrs <thomasamarrs@comcast.net>
2015-11-27 17:17:27 -0700

    Upgraded JSON Patch spec.

--- chapter-7/cities-weather-transform-test/test/json-patch-spec.js
+++ chapter-7/cities-weather-transform-test/test/json-patch-spec.js
@@ -4,19 +4,7 @@ var expect = require('chai').expect;
 var jsonfile = require('jsonfile');
 var jsonpatch = require('json-patch');
 
-var template = [{
-  op: 'add',
-  path: '/submittedSlides',
-  value: true
-}, {
-  op: 'remove',
-  path: '/tags'
-}, {
-  op: 'remove',
-  path: '/company'
-}];
-
-var template2 = [{
+var citiesTemplate = [{
   op: 'remove',
   path: '/coord'
 }, {
@@ -27,45 +15,41 @@ var template2 = [{
   path: '/clouds'
 }, {
   op: 'remove',
-  path: '/wind'
-}, {
-  op: 'remove',
   path: '/weather/0/id'
-}, {
-  op: 'remove',
-  path: '/weather/0/main'
 }, {
   op: 'remove',
   path: '/weather/0/icon'
 }, {
   op: 'move',
   from: '/main/temp',
-  path: '/weather/0/temp'
+  path: '/weather/0/currentTemp'
+}, {
+  op: 'move',
+  from: '/main/temp_min',
+  path: '/weather/0/lowTemp'
+}, {
+  op: 'move',
+  from: '/main/temp_max',
+  path: '/weather/0/hiTemp'
+}, {
+  op: 'move',
+  from: '/main/humidity',
+  path: '/weather/0/humidity'
+}, {
+  op: 'move',
+  from: '/weather/0/main',
+  path: '/weather/0/summary'
+}, {
+  op: 'move',
+  from: '/wind/speed',
+  path: '/weather/0/windSpeed'
 }, {
   op: 'remove',
   path: '/main'
-}];
-
-var template3 = [{
-  op: 'remove',
-  path: '/coord'
-}, {
-  op: 'remove',
-  path: '/dt'
-}, {
-  op: 'remove',
-  path: '/clouds'
 }, {
   op: 'remove',
   path: '/wind'
-}, {
-  op: 'remove',
-  path: '/weather'
-}, {
-  op: 'remove',
-  path: '/main'
 }];
-
 
 describe('cities-json-patch', function() {
   var jsonFileName = null;
@@ -74,53 +58,60 @@ describe('cities-json-patch', function() {
   beforeEach(function() {
     var baseDir = __dirname + '/../../data';
 
-    jsonFileName = baseDir + '/speaker.json';
-    jsonCitiesFileName = baseDir + '/cities-weather.json';
+    jsonCitiesFileName = baseDir + '/cities-weather-short.json';
   });
 
-  it('should patch JSON', function(done) {
-    jsonfile.readFile(jsonFileName, function(err, jsonObj) {
-      if (!err) {
-        //console.log('\n\n\n\Original JSON');
-        //console.log(jsonObj);
-        //var output = jsonpatch.apply(jsonObj, template);
-        //console.log('\n\n\n\Patched JSON');
-        //console.log(JSON.stringify(output));
-      } else {
-        throw (err);
-      }
+  it('should patch all cities - fail', function(done) {
+    jsonfile.readFile(jsonCitiesFileName, function(fileReadError,
+      jsonObj) {
+      if (!fileReadError) {
+        try {
+          var output = jsonpatch.apply(jsonObj, citiesTemplate);
 
-      done();
-    });
-  });
-
-  it('should patch cities', function(done) {
-    jsonfile.readFile(jsonCitiesFileName, function(err, jsonObj) {
-      if (!err) {
-        //console.log('\n\n\n\Original JSON');
-        //console.log(jsonObj);
-        var output = [];
-        //var output = jsonpatch.apply(jsonObj, template2);
-        //console.log(jsonObj);
-        //console.log(typeof jsonObj);
-        //console.log('\n\n\n\Patched JSON');
-        //console.log(JSON.stringify(output, null, 2));
-        var length = 0;
-        for (var i in jsonObj['cities']) {
-          length++;
-          output.push(jsonpatch.apply(jsonObj['cities'][i],
-            template3));
-          //console.log(jsonObj['cities'][i]);
+          console.log('\n\n\n\Original JSON');
+          console.log(jsonObj);
+          console.log('\n\n\n\Patched JSON');
+          console.log(JSON.stringify(output, null, 2));
+        } catch (transformError) {
+          console.error(transformError);
+          throw (transformError);
         }
-
-        //console.log(length);
-        //console.log(typeof jsonObj['cities']);
-        //console.log(JSON.stringify(output, null, 2));
       } else {
-        throw (err);
+        console.error(fileReadError);
+        throw (fileReadError);
       }
 
       done();
     });
   });
+
+  it('should patch all cities - success (kind of)', function(done) {
+    jsonfile.readFile(jsonCitiesFileName, function(fileReadError,
+      jsonObj) {
+      if (!fileReadError) {
+        try {
+          console.log('\n\n\n\Original JSON');
+          console.log(jsonObj);
+          var output = [];
+
+          for (var i in jsonObj['cities']) {
+            output.push(jsonpatch.apply(jsonObj['cities'][i],
+              citiesTemplate));
+          }
+
+          console.log('\n\n\n\Patched JSON');
+          console.log(JSON.stringify(output, null, 2));
+        } catch (transformError) {
+          console.error(transformError);
+          throw (transformError);
+        }
+      } else {
+        console.error(fileReadError);
+        throw (fileReadError);
+      }
+
+      done();
+    });
+  });
+
 });
