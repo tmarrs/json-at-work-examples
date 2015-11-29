4e29f6db05ebeb6f3ba7714f40c756d5a93b261e
Tom Marrs <thomasamarrs@comcast.net>
2015-11-29 16:35:23 -0700

    Removed dead code from jsont-spec.js.

--- chapter-7/cities-weather-transform-test/test/jsont-spec.js
+++ chapter-7/cities-weather-transform-test/test/jsont-spec.js
@@ -34,16 +34,6 @@ describe('cities-jsont', function() {
     'cities[*]': '\n{"name": "{$.name}",\n"weather": {\n"temp": {$.main.temp}, \n"description": "{$.weather[0].description}"\n}\n}'
   };
 
-  // transforms[6] works, but introduces a global variable. Ugh.
-  var index = 0;
-  transforms[6] = {
-    'self': '[\n{cities}\n]',
-    'cities[*]': '\n{"name": "{$.name}",\n"weather": {\n"temp": {$.main.temp}, \n"description": "{$.weather[0].description}"\n}\n}{@comma(cities)}',
-    'comma': function(x) {
-      return (++index < x.length) ? ',' : '';
-    }
-  };
-
   transforms[7] = {
     'self': '{ "cities": [{cities}] }',
     'cities[*]': '{"name": "{$.name}", ' +
@@ -69,38 +59,13 @@ describe('cities-jsont', function() {
     var baseDir = __dirname + '/../../data';
 
     jsonCitiesFileName = baseDir + '/cities-weather-short.json';
-    jsonCityFileName = baseDir + '/city-weather.json';
-  });
-
-  it('should transform city JSON data', function(done) {
-    jsonfile.readFile(jsonCityFileName, function(err, jsonObj) {
-      if (!err) {
-        //console.log(jsonT(jsonObj, transforms[3]));
-      } else {
-        throw (err);
-      }
-
-      done();
-    });
   });
 
   it('should transform cities JSON data', function(done) {
-    jsonfile.readFile(jsonCitiesFileName, function(err, jsonObj) {
-      if (!err) {
-        var jsonStr = jsonT(jsonObj, transforms[4]);
-
-        //console.log(jsonStr);
-        /*
-        for (var key in jsonStr) {
-          console.log('jsonStr[' + key + '] = ' + jsonStr[key]);
-        }
-        */
-        //console.log(typeof jsonStr);
-        //var newJsonStr = jsonStr.replace(/}\s*{/gi, '}, {');
-        /*
-        jsonStr.replace(/"\s*""/gi, '}, {');
-        */
-        //console.log(repairJson(jsonStr));
+    jsonfile.readFile(jsonCitiesFileName, function(readFileError,
+      jsonObj) {
+      if (!readFileError) {
+        //console.log(jsonT(jsonObj, transforms[4]));
       } else {
         throw (err);
       }
@@ -110,31 +75,18 @@ describe('cities-jsont', function() {
   });
 
   it('should transform cities JSON data - Part II', function(done) {
-    jsonfile.readFile(jsonCitiesFileName, function(err, jsonObj) {
-      if (!err) {
-        var obj = jsonT(jsonObj, transforms[6]);
-
-        //console.log(JSON.stringify(JSON.parse(obj), null, 2));
-      } else {
-        throw (err);
-      }
-
-      done();
-    });
-  });
-
-  it('should transform cities JSON data - Part III', function(done) {
-    jsonfile.readFile(jsonCitiesFileName, function(err, jsonObj) {
-      if (!err) {
+    jsonfile.readFile(jsonCitiesFileName, function(readFileError,
+      jsonObj) {
+      if (!readFileError) {
         var jsonStr = jsonT(jsonObj, transforms[7]);
 
         //console.log(jsonStr);
         //console.log(typeof jsonStr);
         jsonStr = repairJson(jsonStr);
-        //console.log(repairJson(jsonStr));
+        console.log(repairJson(jsonStr));
         //console.log(JSON.stringify(JSON.parse(jsonStr), null, 2));
       } else {
-        throw (err);
+        throw (readFileError);
       }
 
       done();
