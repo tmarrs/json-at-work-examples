555b53227d552ab9050501505a242dc732217f27
Tom Marrs <thomasamarrs@comcast.net>
2015-12-15 19:39:30 -0700

    Added Mustache spec to convert JSON to HTML.

---
+++ chapter-7/cities-weather-transform-test/test/mustache-spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var expect = require('chai').expect;
+var jsonfile = require('jsonfile');
+var fs = require('fs');
+var mustache = require('mustache');
+
+describe('cities-mustache', function() {
+  var jsonCitiesFileName = null;
+  var htmlTemplateFileName = null;
+
+
+  beforeEach(function() {
+    var baseDir = __dirname + '/../..';
+
+    jsonCitiesFileName = baseDir + '/data/cities-weather-short.json';
+    htmlTemplateFileName = baseDir +
+      '/templates/transform-html.mustache';
+  });
+
+
+  it('should transform cities JSON data to HTML', function(done) {
+    jsonfile.readFile(jsonCitiesFileName, function(readJsonFileError,
+      jsonObj) {
+      if (!readJsonFileError) {
+        fs.readFile(htmlTemplateFileName, 'utf8', function(
+          readTemplateFileError, templateFileData) {
+          if (!readTemplateFileError) {
+            var template = templateFileData.toString();
+            var html = mustache.render(template, jsonObj);
+
+            console.log('\n\n\nHTML Output:\n' + html);
+            done();
+          }
+        });
+      } else {
+        throw (readJsonFileError);
+      }
+    });
+  });
+});
