a7b680370c3b4f4ee79c52100264ed7d550210c8
Tom Marrs <thomasamarrs@comcast.net>
2015-12-15 20:20:44 -0700

    Added Handlebars spec.

---
+++ chapter-7/cities-weather-transform-test/test/handlebars-spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var expect = require('chai').expect;
+var jsonfile = require('jsonfile');
+var fs = require('fs');
+var handlebars = require('handlebars');
+
+describe('cities-handlebars', function() {
+  var jsonCitiesFileName = null;
+  var htmlTemplateFileName = null;
+
+
+  beforeEach(function() {
+    var baseDir = __dirname + '/../..';
+
+    jsonCitiesFileName = baseDir + '/data/cities-weather-short.json';
+    htmlTemplateFileName = baseDir +
+      '/templates/transform-full-html.hbs';
+  });
+
+
+  it('should transform cities JSON data to HTML', function(done) {
+    jsonfile.readFile(jsonCitiesFileName, function(readJsonFileError,
+      jsonObj) {
+      if (!readJsonFileError) {
+        fs.readFile(htmlTemplateFileName, 'utf8', function(
+          readTemplateFileError, templateFileData) {
+          if (!readTemplateFileError) {
+            var template = handlebars.compile(templateFileData);
+            var html = template(jsonObj);
+
+            console.log('\n\n\nHTML Output:\n' + html);
+            done();
+          } else {
+            throw (readTemplateFileError);
+          }
+        });
+      } else {
+        throw (readJsonFileError);
+      }
+    });
+  });
+});
