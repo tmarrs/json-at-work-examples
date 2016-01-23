8ada80e40345b95bca4bba699a508fb1bb14cbb8
Tom Marrs <thomasamarrs@comcast.net>
2016-01-23 12:39:01 -0700

    Created Java project.

---
+++ chapter-4/java-json/src/main/java/org/jsonatwork/ch4/App.java
@@ -0,0 +1,13 @@
+package org.jsonatwork.ch4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
