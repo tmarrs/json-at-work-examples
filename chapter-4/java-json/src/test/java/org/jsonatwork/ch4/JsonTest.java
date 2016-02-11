f11811ebe3a311f85edb6e6d4ee9c92cc2469023
Tom Marrs <thomasamarrs@comcast.net>
2016-02-10 21:27:00 -0700

    Roughed in JUnit and Jackson.

---
+++ chapter-4/java-json/src/test/java/org/jsonatwork/ch4/JsonTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.jsonatwork.ch4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author tmarrs
+ *
+ */
+public class JsonTest {
+
+	@Test
+	public void testBasicDataTypes() {
+		assertTrue(true);
+	}
+
+}
