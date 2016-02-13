32c65b5406c7e0f319c628578b94828b9fd29361
Tom Marrs <thomasamarrs@comcast.net>
2016-02-13 12:59:17 -0700

    Upgraded code

--- chapter-4/java-json/src/test/java/org/jsonatwork/ch4/BasicJsonTypesTest.java
+++ chapter-4/java-json/src/test/java/org/jsonatwork/ch4/BasicJsonTypesTest.java
@@ -6,14 +6,10 @@ package org.jsonatwork.ch4;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
 
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author tmarrs
@@ -21,44 +17,68 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
  */
 public class BasicJsonTypesTest {
 
+	
 	@Test
-	public void testBasicDataTypes() {
+	public void testTypes() {
 		try {
+			ObjectMapper mapper = new ObjectMapper();
 			int age = 39;
 			String fullName = new String("Larson Richard");
 			String[] tags = new String[] { "json", "rest", "api", "oauth" };
-			Writer writer = new StringWriter();
+			boolean registered = true;
 
-			JsonFactory factory = new JsonFactory();
-			JsonGenerator jsonGenerator = factory.createGenerator(writer);
-
-			jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter());
-
-			jsonGenerator.writeStartObject();
-			jsonGenerator.writeNumberField("age", age);
-			jsonGenerator.writeStringField("fullName", fullName);
-			jsonGenerator.writeArrayFieldStart("tags");
-			for (String tag : tags) {
-				jsonGenerator.writeString(tag);
-			}
-			jsonGenerator.writeEndArray();
-			jsonGenerator.writeEndObject();
-
-			jsonGenerator.close();
-			System.out.println(writer.toString());
+			
+			System.out.println("age = " + mapper.writeValueAsString(age));
+			System.out.println("\nfullName = " +  mapper.writeValueAsString(fullName));
+			System.out.println("\ntags = " +  mapper.writeValueAsString(tags));
+			System.out.println("\nregistered = " +  mapper.writeValueAsString(registered));
 			assertTrue(true);
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 			fail(ioe.getMessage());
 		}
+		
 	}
 }
 
+
 /*
- * import com.fasterxml.jackson.databind.ObjectMapper; ObjectMapper mapper = new
- * ObjectMapper();
- * 
- * try { mapper.writeValue(writer, age); System.out.println(writer.toString());
- * } catch (IOException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); fail(e.getMessage()); }
- */
+import java.io.StringWriter;
+import java.io.Writer;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+
+@Test
+public void testBasicDataTypes() {
+	try {
+		int age = 39;
+		String fullName = new String("Larson Richard");
+		String[] tags = new String[] { "json", "rest", "api", "oauth" };
+		Writer writer = new StringWriter();
+
+		JsonFactory factory = new JsonFactory();
+		JsonGenerator jsonGenerator = factory.createGenerator(writer);
+
+		jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter());
+
+		jsonGenerator.writeStartObject();
+		jsonGenerator.writeNumberField("age", age);
+		jsonGenerator.writeStringField("fullName", fullName);
+		jsonGenerator.writeArrayFieldStart("tags");
+		for (String tag : tags) {
+			jsonGenerator.writeString(tag);
+		}
+		jsonGenerator.writeEndArray();
+		jsonGenerator.writeEndObject();
+
+		jsonGenerator.close();
+		System.out.println(writer.toString());
+		assertTrue(true);
+	} catch (IOException ioe) {
+		ioe.printStackTrace();
+		fail(ioe.getMessage());
+	}
+}
+*/
