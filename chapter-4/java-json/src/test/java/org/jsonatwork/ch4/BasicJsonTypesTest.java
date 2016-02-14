5a79efd18dd3832b594ac186e7429b7b46f3627e
Tom Marrs <thomasamarrs@comcast.net>
2016-02-13 17:13:39 -0700

    Added de-serialization for basic types.

--- chapter-4/java-json/src/test/java/org/jsonatwork/ch4/BasicJsonTypesTest.java
+++ chapter-4/java-json/src/test/java/org/jsonatwork/ch4/BasicJsonTypesTest.java
@@ -6,9 +6,16 @@ package org.jsonatwork.ch4;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -19,25 +26,57 @@ public class BasicJsonTypesTest {
 
 	
 	@Test
-	public void testTypes() {
+	public void serializeBasicTypes() {
 		try {
 			ObjectMapper mapper = new ObjectMapper();
+			Writer writer = new StringWriter();
 			int age = 39;
 			String fullName = new String("Larson Richard");
 			String[] tags = new String[] { "json", "rest", "api", "oauth" };
 			boolean registered = true;
 
-			
-			System.out.println("age = " + mapper.writeValueAsString(age));
-			System.out.println("\nfullName = " +  mapper.writeValueAsString(fullName));
-			System.out.println("\ntags = " +  mapper.writeValueAsString(tags));
-			System.out.println("\nregistered = " +  mapper.writeValueAsString(registered));
+			writer.write("age = ");
+			mapper.writeValue(writer, age);
+			writer.write("\nfullName = ");
+			mapper.writeValue(writer, fullName);
+			writer.write("\ntags = ");
+			mapper.writeValue(writer, tags);
+			writer.write("\nregistered = ");
+			mapper.writeValue(writer, registered);
+			System.out.println(writer.toString());
 			assertTrue(true);
+		} catch (JsonGenerationException jge) {
+			jge.printStackTrace();
+			fail(jge.getMessage());
+		} catch (JsonMappingException jme) {
+			jme.printStackTrace();
+			fail(jme.getMessage());
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			fail(ioe.getMessage());
+		}		
+	}
+	
+	
+	@Test
+	public void deSerializeBasicTypes() {	
+		try {
+			
+			String ageJson = "{ \"age\": 39 }";
+			ObjectMapper mapper = new ObjectMapper();
+			
+			Map<String, Integer> ageMap = mapper.readValue(ageJson, 
+					                                new TypeReference<HashMap<String,Integer>>() {});
+			
+			System.out.println("age = " + ageMap.get("age") + "\n\n\n");
+            assertTrue(true);
+		} catch (JsonMappingException jme) {
+			jme.printStackTrace();
+			fail(jme.getMessage());
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 			fail(ioe.getMessage());
 		}
-		
 	}
 }
 
