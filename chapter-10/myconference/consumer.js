6af0c9c2c6a4da08149a63b975dd2ff06aa6684e
Tom Marrs <thomasamarrs@comcast.net>
2017-01-07 19:03:19 -0700

    Fixed topic name.

--- chapter-10/myconference/consumer.js
+++ chapter-10/myconference/consumer.js
@@ -4,7 +4,7 @@ var kafka = require('kafka-node');
 var client = new kafka.Client(),
   consumer = new kafka.Consumer(
     client, [{
-      topic: 'new-topics-recvd'
+      topic: 'new-proposals-recvd'
     }], {
       autoCommit: false
     }
@@ -12,4 +12,9 @@ var client = new kafka.Client(),
 
 consumer.on('message', function(message) {
   console.log("received message", message);
+});
+
+
+consumer.on('error', function(err) {
+  console.log(err);
 });
