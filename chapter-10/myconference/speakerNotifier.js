8d7f9905b50424aba784ba50f680c8e80d7c983c
Tom Marrs <thomasamarrs@comcast.net>
2017-01-09 12:06:54 -0700

    Live message goes all the way through to email.

--- chapter-10/myconference/speakerNotifier.js
+++ chapter-10/myconference/speakerNotifier.js
@@ -6,6 +6,7 @@ var nodeMailer = require('nodemailer');
 const PROPOSALS_REVIEWED_TOPIC = 'proposals-reviewed';
 const MAILCATCHER_SMTP_HOST = 'localhost';
 const MAILCATCHER_SMTP_PORT = 1025;
+const EMAIL_FROM = '"My Conference" <proposals@myconference.com>';
 
 var consumerClient = new kafka.Client(),
   consumer = new kafka.Consumer(
@@ -25,24 +26,37 @@ var transporter = nodeMailer.createTransport(mailCatcherSmtpConfig);
 
 consumer.on('message', function(message) {
   console.log('received message', message);
-  sendEmail();
+  notifySpeaker(message.value);
 });
 
+function notifySpeaker(message) {
+  var notificationMessage = createNotificationMessage(message);
+
+  sendEmail(notificationMessage);
+}
+
 // FIXME: Remove hardcoding.
-// Use constant for the "from".
 // Pull the "to" from the incoming JSON message from the topic.
 // Generate the "subject" from the incoming JSON message from the topic. Accepted/Rejected.
 // Generate email body from incoming JSON message. Use handlebars. Fix up HTML message. Use core HTML 
 // template: http://www.w3schools.com/html/html5_intro.asp
 // Use JSON.parse() and JSON.stringify() to process JSON.
-function sendEmail() {
+function createNotificationMessage(message) {
+  return createMailOptions(message);
+}
+
+function createMailOptions(message) {
   var mailOptions = {
-    from: '"My Conference" <proposals@myconference.com>', // sender address 
+    from: EMAIL_FROM, // sender address 
     to: 'fred.smith@acme.com', // list of receivers 
     subject: 'Hello', // Subject line 
-    html: '<b>Hello world</b>' // html body 
+    html: '<b>' + message + '</b>' // html body 
   };
 
+  return mailOptions;
+}
+
+function sendEmail(mailOptions) {
   // send mail with defined transport object 
   transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
