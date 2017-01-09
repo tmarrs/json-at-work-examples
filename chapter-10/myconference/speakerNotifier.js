c69b2263f85301411fa589cdec2ca75891b9259d
Tom Marrs <thomasamarrs@comcast.net>
2017-01-09 12:33:26 -0700

    Chose handlebars npm module.

--- chapter-10/myconference/speakerNotifier.js
+++ chapter-10/myconference/speakerNotifier.js
@@ -2,11 +2,13 @@
 
 var kafka = require('kafka-node');
 var nodeMailer = require('nodemailer');
+var handlebars = require('handlebars');
 
 const PROPOSALS_REVIEWED_TOPIC = 'proposals-reviewed';
 const MAILCATCHER_SMTP_HOST = 'localhost';
 const MAILCATCHER_SMTP_PORT = 1025;
 const EMAIL_FROM = '"My Conference" <proposals@myconference.com>';
+
 
 var consumerClient = new kafka.Client(),
   consumer = new kafka.Consumer(
@@ -26,6 +28,7 @@ var transporter = nodeMailer.createTransport(mailCatcherSmtpConfig);
 
 consumer.on('message', function(message) {
   console.log('received message', message);
+
   notifySpeaker(message.value);
 });
 
@@ -42,10 +45,12 @@ function notifySpeaker(message) {
 // template: http://www.w3schools.com/html/html5_intro.asp
 // Use JSON.parse() and JSON.stringify() to process JSON.
 function createNotificationMessage(message) {
+  console.log('Notification Message: ' + message);
   return createMailOptions(message);
 }
 
 function createMailOptions(message) {
+  var messageAsObj = JSON.parse(message);
   var mailOptions = {
     from: EMAIL_FROM, // sender address 
     to: 'fred.smith@acme.com', // list of receivers 
