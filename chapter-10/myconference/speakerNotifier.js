73fbcff90592a1555f2f9be2b0525d72e05894f0
Tom Marrs <thomasamarrs@comcast.net>
2017-01-10 22:19:38 -0700

    Upgraded speakerNotifier so that it renders a template to generate the email message body.

--- chapter-10/myconference/speakerNotifier.js
+++ chapter-10/myconference/speakerNotifier.js
@@ -3,13 +3,19 @@
 var kafka = require('kafka-node');
 var nodeMailer = require('nodemailer');
 var handlebars = require('handlebars');
-var util = require('util');
+var fs = require('fs');
 
 const PROPOSALS_REVIEWED_TOPIC = 'proposals-reviewed';
 const MAILCATCHER_SMTP_HOST = 'localhost';
 const MAILCATCHER_SMTP_PORT = 1025;
-const EMAIL_FROM = '"My Conference" <proposals@myconference.com>';
+const EMAIL_FROM = 'proposals@myconference.com';
+const ACCEPTED_PROPOSAL_HB_TEMPLATE_FILE_NAME =
+  './templates/acceptedProposal.hbs';
 
+const REJECTED_PROPOSAL_HB_TEMPLATE_FILE_NAME =
+  './templates/rejectedProposal.hbs';
+
+const UTF_8 = 'utf8';
 
 var consumerClient = new kafka.Client(),
   consumer = new kafka.Consumer(
@@ -39,29 +45,34 @@ function notifySpeaker(notification) {
   sendEmail(notificationMessage);
 }
 
-// FIXME:
-// Generate email body from incoming JSON message. Use handlebars.
 function createNotificationMessage(notification) {
-  console.log('Notification Message = ' + notification);
-  return createMailOptions(notification);
-}
-
-function createMailOptions(notification) {
   var notificationAsObj = JSON.parse(notification);
   var proposal = notificationAsObj.proposal;
+
+  console.log('Notification Message = ' + notification);
 
   var mailOptions = {
     from: EMAIL_FROM, // sender address 
     to: proposal.speaker.email, // list of receivers 
     subject: proposal.conference.name + ' - ' + proposal.session.title, // Subject line 
-    html: createEmailBody(notification)
+    html: createEmailBody(notificationAsObj)
   };
 
   return mailOptions;
 }
 
 function createEmailBody(notification) {
-  return '<b>Hi There</b>';
+  // Read Handlebars Template file.
+  var hbTemplateContent = fs.readFileSync(((notification.decision.accepted) ?
+    ACCEPTED_PROPOSAL_HB_TEMPLATE_FILE_NAME :
+    REJECTED_PROPOSAL_HB_TEMPLATE_FILE_NAME), UTF_8);
+
+  // Compile the template into a function.
+  var template = handlebars.compile(hbTemplateContent);
+  var body = template(notification); // Render the template.
+
+  console.log('Email body = ' + body);
+  return body;
 }
 
 function sendEmail(mailOptions) {
