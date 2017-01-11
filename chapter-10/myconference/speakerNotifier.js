0cbc6171c6c5a004d63ad5ad0f90bc04d8949459
Tom Marrs <thomasamarrs@comcast.net>
2017-01-10 21:23:14 -0700

    SpeakerNotifier now handles incoming JSON message properly.

--- chapter-10/myconference/speakerNotifier.js
+++ chapter-10/myconference/speakerNotifier.js
@@ -3,6 +3,7 @@
 var kafka = require('kafka-node');
 var nodeMailer = require('nodemailer');
 var handlebars = require('handlebars');
+var util = require('util');
 
 const PROPOSALS_REVIEWED_TOPIC = 'proposals-reviewed';
 const MAILCATCHER_SMTP_HOST = 'localhost';
@@ -32,33 +33,35 @@ consumer.on('message', function(message) {
   notifySpeaker(message.value);
 });
 
-function notifySpeaker(message) {
-  var notificationMessage = createNotificationMessage(message);
+function notifySpeaker(notification) {
+  var notificationMessage = createNotificationMessage(notification);
 
   sendEmail(notificationMessage);
 }
 
-// FIXME: Remove hardcoding.
-// Pull the "to" from the incoming JSON message from the topic.
-// Generate the "subject" from the incoming JSON message from the topic. Accepted/Rejected.
-// Generate email body from incoming JSON message. Use handlebars. Fix up HTML message. Use core HTML 
-// template: http://www.w3schools.com/html/html5_intro.asp
-// Use JSON.parse() and JSON.stringify() to process JSON.
-function createNotificationMessage(message) {
-  console.log('Notification Message: ' + message);
-  return createMailOptions(message);
+// FIXME:
+// Generate email body from incoming JSON message. Use handlebars.
+function createNotificationMessage(notification) {
+  console.log('Notification Message = ' + notification);
+  return createMailOptions(notification);
 }
 
-function createMailOptions(message) {
-  var messageAsObj = JSON.parse(message);
+function createMailOptions(notification) {
+  var notificationAsObj = JSON.parse(notification);
+  var proposal = notificationAsObj.proposal;
+
   var mailOptions = {
     from: EMAIL_FROM, // sender address 
-    to: 'fred.smith@acme.com', // list of receivers 
-    subject: 'Hello', // Subject line 
-    html: '<b>' + message + '</b>' // html body 
+    to: proposal.speaker.email, // list of receivers 
+    subject: proposal.conference.name + ' - ' + proposal.session.title, // Subject line 
+    html: createEmailBody(notification)
   };
 
   return mailOptions;
+}
+
+function createEmailBody(notification) {
+  return '<b>Hi There</b>';
 }
 
 function sendEmail(mailOptions) {
