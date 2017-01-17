a68a3c8c85317584f5776901aed3cda8845fc99a
Tom Marrs <thomasamarrs@comcast.net>
2017-01-17 13:48:32 -0700

    Made the app more robust. Replaced Consumer with ConsumerGroup, Producer with HighLevelProducer, and forced a close/commit on shutdown.

--- chapter-10/myconference/proposalReviewer.js
+++ chapter-10/myconference/proposalReviewer.js
@@ -9,23 +9,34 @@ const PROPOSALS_REVIEWED_TOPIC = 'proposals-reviewed';
 const SPEAKER_PROPOSAL_SCHEMA_FILE_NAME =
   './schemas/speakerProposalSchema.json';
 
-var consumerClient = new kafka.Client(),
-  consumer = new kafka.Consumer(
-    consumerClient, [{
-      topic: NEW_PROPOSALS_RECEIVED_TOPIC
-    }], {
-      autoCommit: true
-    }
-  );
+var consumer = new kafka.ConsumerGroup({
+  fromOffset: 'latest',
+  autoCommit: true
+}, NEW_PROPOSALS_RECEIVED_TOPIC);
 
 var producerClient = new kafka.Client(),
-  producer = new kafka.Producer(producerClient);
+  producer = new kafka.HighLevelProducer(producerClient);
 
 // Use incoming JSON message.
 // Use JSON.parse() and JSON.stringify() to process JSON.
 consumer.on('message', function(message) {
   console.log('received message', message);
   processProposal(message);
+});
+
+process.on('SIGINT', function() {
+  console.log(
+    'SIGINT received - Proposal Reviewer closing. Committing current offset on Topic: ' +
+    NEW_PROPOSALS_RECEIVED_TOPIC + ' ...'
+  );
+
+  consumer.close(true, function() {
+    console.log(
+      'Finished committing current offset. Exiting with graceful shutdown ...'
+    );
+
+    process.exit();
+  });
 });
 
 function processProposal(proposal) {
