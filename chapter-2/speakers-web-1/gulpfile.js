7788dc6bb102e1dcf5d8db8e59363907a4453537
Tom Marrs <thomasamarrs@comcast.net>
2017-03-18 16:18:08 -0600

    Re-did speakers-web-1 app with new gulp-based generator-webapp.

--- chapter-2/speakers-web-1/gulpfile.js
+++ chapter-2/speakers-web-1/gulpfile.js
@@ -1,4 +1,4 @@
-// generated on 2017-03-17 using generator-webapp 2.4.1
+// generated on 2017-03-18 using generator-webapp 2.4.1
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
