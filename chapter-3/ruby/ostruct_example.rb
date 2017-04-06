841f8afce526c412f4d297c09fdce26e9cb325fd
Tom Marrs <thomasamarrs@comcast.net>
2017-04-05 20:51:53 -0600

    Replaced hashie with OpenStruct.

--- chapter-3/ruby/ostruct_example.rb
+++ chapter-3/ruby/ostruct_example.rb
@@ -1,6 +1,6 @@
-require 'hashie'
+require 'ostruct'
 
 h = { first_name: 'Fred' }
-m = Hashie::Mash.new(h)
+m = OpenStruct.new(h)
 puts m             # prints: #<Hashie::Mash first_name="Fred">
 puts m.first_name  # prints: Fred
