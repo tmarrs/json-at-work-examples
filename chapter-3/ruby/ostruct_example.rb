8c2dcf9edf4b1d9259d525fe876eccf801565683
Tom Marrs <thomasamarrs@comcast.net>
2017-04-06 17:22:16 -0600

    Fixed comment.

--- chapter-3/ruby/ostruct_example.rb
+++ chapter-3/ruby/ostruct_example.rb
@@ -2,5 +2,5 @@ require 'ostruct'
 
 h = { first_name: 'Fred' }
 m = OpenStruct.new(h)
-puts m             # prints: #<Hashie::Mash first_name="Fred">
+puts m             # prints: #<OpenStruct first_name="Fred">
 puts m.first_name  # prints: Fred
