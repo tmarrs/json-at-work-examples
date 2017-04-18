1365da9da0ea3b399635c0857056baaac6952290
Tom Marrs <thomasamarrs@comcast.net>
2017-04-17 18:02:02 -0600

    Deleted unused code.

--- chapter-3/ror/samples/speaker.rb
+++
@@ -1,6 +0,0 @@
-class Speaker < ActiveRecord::Base
-  def as_json(options={})
-    options[:except] ||= [:picture, :email]
-    super
-  end
-end
--- chapter-3/ror/samples/speakers_contoller_default.rb
+++
@@ -1,7 +0,0 @@
-class SpeakersController < ApplicationController
-  def show
-    @speaker = Speaker.find(params[:id])
-    
-    render json: @speaker
-  end
-end
